bd2b82b3093f94a1fb87c73886f77d1b9114f685
JMaue <j.mauersberger@freenet.de>
2025-01-21 21:34:27 +0100

    more experiments

--- Str8tsSolverImageTools/BoardFinder.cs
+++ Str8tsSolverImageTools/BoardFinder.cs
@@ -239,6 +239,11 @@ namespace Str8tsSolverImageTools
         CvInvoke.PutText(image, "4", upperLeft, FontFace.HersheyPlain, 8, new MCvScalar(255, 0, 255), 4);
       }
 
+      var grayImage = new Mat();
+      CvInvoke.CvtColor(image, grayImage, ColorConversion.Bgr2Gray);
+
+      (bool[,] blackValues, int average, int avgBlack, int avgWhite) = CalcBlackWhiteMatrix (grayImage, upperLeft, upperRight, lowerLeft, lowerRight);
+
       var dxr = (upperRight.X - lowerRight.X) / 9;
       var dyr = (lowerRight.Y - upperRight.Y) / 9;    // always positiv
       var dxl = (upperLeft.X - lowerLeft.X) / 9;
@@ -280,18 +285,13 @@ namespace Str8tsSolverImageTools
           int shrink = Convert.ToInt16(dxt * 0.20); // take 15% off from all sides
           Rectangle rect = new Rectangle(topLeft.X + shrink, topLeft.Y + shrink, topRight.X - topLeft.X - (2 * shrink), bottomLeft.Y - topLeft.Y - (2 * shrink));
 
-          // Durchschnittlichen Grauwert berechnen
-          var grayImage = new Mat();
-          CvInvoke.CvtColor(image, grayImage, ColorConversion.Bgr2Gray);
-          var averageGrayValue = GetAverageGrayValue(grayImage, rect);
-          var black = averageGrayValue < 100;
-
+          var isBlack = blackValues[r, c];
           Mat roi = new Mat(grayImage, rect);
-          var digit = GetDigit(ref roi, r, c, isScreenShot);
+          var digit = isBlack ? GetDigitBlack (roi, r, c, isScreenShot) : GetDigitWhite(roi, r, c, avgWhite, isScreenShot);
           SaveRegionToFile(roi, @$"D:\Jens\Repositories\Str8tsSolver\Data\{r}{c}.png");
           if (digit >= 1 && digit <= 9)
           {
-            if (black)
+            if (isBlack)
               board[r, c] = (char)('A' + digit - 1);
             else
               board[r, c] = (char)('0' + digit);
@@ -301,17 +301,92 @@ namespace Str8tsSolverImageTools
             CvInvoke.PutText(image, $"{digit}", bottomLeft, FontFace.HersheyPlain, fontScale, new MCvScalar(0, 255, 0), thickness);
           }
           else
-            if (black)
-            board[r, c] = '#';
-          else
-            board[r, c] = ' ';
+            if (isBlack)
+              board[r, c] = '#';
+            else
+              board[r, c] = ' ';
 
           _points[r, c] = bottomLeft;
-          //CvInvoke.PutText(image, $"{agv}", bottomLeft, FontFace.HersheyPlain, 4, new MCvScalar(0, 255, 0), 4);
         }
       }
 
       return board;
+    }
+
+    private (bool[,], int, int, int) CalcBlackWhiteMatrix(Mat image, Point upperLeft, Point upperRight, Point lowerLeft, Point lowerRight)
+    {
+      var dxr = (upperRight.X - lowerRight.X) / 9;
+      var dyr = (lowerRight.Y - upperRight.Y) / 9;    // always positiv
+      var dxl = (upperLeft.X - lowerLeft.X) / 9;
+      var dyl = (lowerLeft.Y - upperLeft.Y) / 9;      // always positiv
+
+      var board = new bool[9, 9];
+      double graySum = 0;
+      void ProcessRectangles (Action<double, int, int> processAverage)
+      {
+        for (int r = 0; r < 9; r++)
+        {
+          var tl = new Point(upperLeft.X - r * dxl, upperLeft.Y + r * dyl);
+          var tr = new Point(upperRight.X - r * dxr, upperRight.Y + r * dyr);
+          var bl = new Point(upperLeft.X - (r + 1) * dxl, upperLeft.Y + (r + 1) * dyl);
+          var br = new Point(upperRight.X - (r + 1) * dxr, upperRight.Y + (r + 1) * dyr);
+          var dxt = (tr.X - tl.X) / 9;
+          var dyt = (tr.Y - tl.Y) / 9;
+          var dxb = (br.X - bl.X) / 9;
+          var dyb = (br.Y - bl.Y) / 9;
+          for (int c = 0; c < 9; c++)
+          {
+            // Berechne die Eckpunkte des Rechtecks
+            var topLeft = new Point(tl.X + c * dxt, tl.Y + c * dyt);
+            var topRight = new Point(tl.X + (c + 1) * dxt, tl.Y + (c + 1) * dyt);
+            var bottomLeft = new Point(bl.X + c * dxb, bl.Y + c * dyb);
+            var bottomRight = new Point(bl.X + (c + 1) * dxb, bl.Y + (c + 1) * dyb);
+
+            ySize = bottomLeft.Y - topLeft.Y;
+
+            // Rechteck definieren
+            int shrink = Convert.ToInt16(dxt * 0.20); // take 15% off from all sides
+            Rectangle rect = new Rectangle(topLeft.X + shrink, topLeft.Y + shrink, topRight.X - topLeft.X - (2 * shrink), bottomLeft.Y - topLeft.Y - (2 * shrink));
+
+            // Durchschnittlichen Grauwert berechnen
+            //var grayImage = new Mat();
+            //CvInvoke.CvtColor(image, grayImage, ColorConversion.Bgr2Gray);
+            var average = GetAverageGrayValue(image, rect);
+            processAverage(average, r, c);
+          }
+        }
+      }
+      int[] vals = new int[256];
+      ProcessRectangles((average, r, c) => {
+        graySum += average;
+        vals[Convert.ToInt32(average)]++;
+      });
+      int mean = Convert.ToInt32(graySum / 81);
+      int avgWhite = 0;
+      int noWhite = 0;
+      int avgBlack = 0;
+      int noBlack = 0;
+      for (int i=0; i<256; i++)
+      {
+        if (vals[i] == 0)
+          continue; 
+        if (i > mean)
+        {
+          avgWhite += i*vals[i];
+          noWhite += vals[i];
+        }
+        else
+        {
+          avgBlack += i * vals[i];
+          noBlack += vals[i];
+        }
+      }
+      avgBlack /= noBlack;
+      avgWhite /= noWhite;
+
+      ProcessRectangles((average, r, c) => board[r, c] = average < mean);
+
+      return (board, mean, avgBlack, avgWhite);
     }
 
     public static double GetAverageGrayValue(Mat grayImage, Rectangle rect)
@@ -328,48 +403,13 @@ namespace Str8tsSolverImageTools
       return averageGrayValue;
     }
 
-    public int GetDigit(ref Mat grayImage, int r, int c, bool isScreenShot)
+    public int GetDigitWhite(Mat roi, int r, int c, int averageWhiteVal, bool isScreenShot)
     {
-      Mat img4Ocr = grayImage;
-      DenseHistogram histogram = new DenseHistogram(256, new RangeF(0, 256));
-
-      Image<Gray, byte> graySmooth = new Image<Gray, byte>(grayImage.Size);
-      if (!isScreenShot)
-      {
-        var increasedContrast = new Mat();
-        CvInvoke.ConvertScaleAbs(grayImage, increasedContrast, 1.5, 5);
-        graySmooth = increasedContrast.ToImage<Gray, byte>().SmoothGaussian(45, 45, 2.5, 2.5);
-
-        //DenseHistogram histogram = new DenseHistogram(256, new RangeF(0, 256));
-        histogram.Calculate(new Image<Gray, byte>[] { img4Ocr.ToImage<Gray, byte>() }, false, null);
-
-        var averageGrayValue = GetAverageGrayValue(grayImage, new Rectangle(0, 0, grayImage.Cols - 1, grayImage.Rows - 1));
-        var black = averageGrayValue < 100;
-
-        Mat imgThresholded = new Mat();
-        double binThreshold;
-        if (black && isScreenShot)
-          binThreshold = 150;
-        else if (black && !isScreenShot)
-          binThreshold = 150;
-        else if (!black && isScreenShot)
-          binThreshold = 150;
-        else
-          binThreshold = 180;
-
-        if (black)
-          CvInvoke.Threshold(graySmooth, imgThresholded, 0, 255, ThresholdType.Binary | ThresholdType.Otsu);
-        else
-          CvInvoke.AdaptiveThreshold(graySmooth, imgThresholded, 255, AdaptiveThresholdType.MeanC, ThresholdType.Binary, 11, 2);
-        
-        //CvInvoke.Threshold(graySmooth, imgThresholded, binThreshold, 255, ThresholdType.Binary);
-        img4Ocr = imgThresholded;
-        //img4Ocr = graySmooth.Mat;
-      }
+      Mat img4Ocr = roi;
 
       #region Berechnung des Histogramms für das Graubild
-
-      histogram.Calculate(new Image<Gray, byte>[] { graySmooth /* img4Ocr.ToImage<Gray, byte>() */ }, false, null);
+      DenseHistogram histogram = new DenseHistogram(256, new RangeF(0, 256));
+      histogram.Calculate(new Image<Gray, byte>[] { img4Ocr.ToImage<Gray, byte>() }, false, null);
 
       // Optional: Um das Histogramm anzuzeigen
       Mat histImage = new Mat();
@@ -387,6 +427,24 @@ namespace Str8tsSolverImageTools
       CvInvoke.Imwrite(filePath, histImage);
       #endregion
 
+      Image<Gray, byte> graySmooth = new Image<Gray, byte>(roi.Size);
+      if (!isScreenShot)
+      {
+        var increasedContrast = new Mat();
+        CvInvoke.ConvertScaleAbs(roi, increasedContrast, 1.5, 5);
+        graySmooth = increasedContrast.ToImage<Gray, byte>().SmoothGaussian(45, 45, 2.5, 2.5);
+        img4Ocr = graySmooth.Mat;
+
+        Mat imgThresholded = new Mat();
+        double binThreshold = isScreenShot ? 150 : 180;
+
+        CvInvoke.Threshold(img4Ocr, imgThresholded, averageWhiteVal*1.2, 255, ThresholdType.Binary);
+        
+        //CvInvoke.Threshold(graySmooth, imgThresholded, binThreshold, 255, ThresholdType.Binary);
+        img4Ocr = imgThresholded;
+        //img4Ocr = graySmooth.Mat;
+      }
+
       var txt = ExtractDigitsFromImage(img4Ocr);
       txt = txt.Replace("\r\n", "");
       int val = -1;
@@ -397,7 +455,65 @@ namespace Str8tsSolverImageTools
           val = d - '0';
       }
 
-      CvInvoke.PutText(img4Ocr, $"{txt}", new Point(3, grayImage.Height - 3), FontFace.HersheyPlain, 4, new MCvScalar(111, 111, 111), 4);
+      CvInvoke.PutText(img4Ocr, $"{txt}", new Point(3, roi.Height - 3), FontFace.HersheyPlain, 4, new MCvScalar(111, 111, 111), 4);
+      SaveRegionToFile(img4Ocr, @$"D:\Jens\Repositories\Str8tsSolver\Data\{r}{c}c.png");
+
+      return val;
+    }
+
+    public int GetDigitBlack (Mat roi, int r, int c, bool isScreenShot)
+    {
+      Mat img4Ocr = roi;
+
+      #region Berechnung des Histogramms für das Graubild
+      DenseHistogram histogram = new DenseHistogram(256, new RangeF(0, 256));
+      histogram.Calculate(new Image<Gray, byte>[] { img4Ocr.ToImage<Gray, byte>() }, false, null);
+
+      // Optional: Um das Histogramm anzuzeigen
+      Mat histImage = new Mat();
+      CvInvoke.Normalize(histogram, histogram, 0, 255, NormType.MinMax);
+      histImage = new Mat(256, 256, DepthType.Cv8U, 1);
+      histImage.SetTo(new MCvScalar(255));
+      for (int i = 0; i < 256; i++)
+      {
+        int binVal = (int)histogram.GetBinValues()[i];
+        CvInvoke.Line(histImage, new Point(i, 256), new Point(i, 256 - binVal), new MCvScalar(0, 255, 0), 2);
+      }
+
+      // Speichern Sie das Histogramm-Bild als PNG-Datei
+      string filePath = @$"D:\Jens\Repositories\Str8tsSolver\Data\{r}{c}h.png";
+      CvInvoke.Imwrite(filePath, histImage);
+      #endregion
+
+      Image<Gray, byte> graySmooth = new Image<Gray, byte>(roi.Size);
+      if (!isScreenShot)
+      {
+        //var increasedContrast = new Mat();
+        //CvInvoke.ConvertScaleAbs(roi, increasedContrast, 1.5, 5);
+        //graySmooth = increasedContrast.ToImage<Gray, byte>().SmoothGaussian(45, 45, 2.5, 2.5);
+        //img4Ocr = graySmooth.Mat;
+
+        //Mat imgThresholded = new Mat();
+        //double binThreshold = isScreenShot ? 150 : 180;
+
+        CvInvoke.Threshold(roi, img4Ocr, 0, 255, ThresholdType.Binary | ThresholdType.Otsu);
+
+        //CvInvoke.Threshold(graySmooth, imgThresholded, binThreshold, 255, ThresholdType.Binary);
+        //img4Ocr = roi;
+        //img4Ocr = graySmooth.Mat;
+      }
+
+      var txt = ExtractDigitsFromImage(img4Ocr);
+      txt = txt.Replace("\r\n", "");
+      int val = -1;
+      if (txt.Count() <= 3)
+      {
+        var d = txt.FirstOrDefault(c => c >= '1' && c <= '9');
+        if (d > 0)
+          val = d - '0';
+      }
+
+      CvInvoke.PutText(img4Ocr, $"{txt}", new Point(3, roi.Height - 3), FontFace.HersheyPlain, 4, new MCvScalar(111, 111, 111), 4);
       SaveRegionToFile(img4Ocr, @$"D:\Jens\Repositories\Str8tsSolver\Data\{r}{c}c.png");
 
       return val;
