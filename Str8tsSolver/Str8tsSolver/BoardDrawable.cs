07a9752420c4b5cdc194dc96aca68bf98c2c1f93
JMaue <j.mauersberger@freenet.de>
2025-02-13 12:20:26 +0100

    added CaptureImageThread

--- Str8tsSolver/Str8tsSolver/BoardDrawable.cs
+++ Str8tsSolver/Str8tsSolver/BoardDrawable.cs
@@ -9,11 +9,18 @@ namespace Str8tsSolver
 {
   public class BoardDrawable : IDrawable
   {
+    private int _x;
+    private int _y;
+    public void UpdatePosition (int  x, int y)
+    {
+      _x = x;
+      _y = y;
+    }
     public void Draw(ICanvas canvas, RectF dirtyRect)
     {
       canvas.StrokeColor = Colors.Red;
       canvas.StrokeSize = 4;
-      var rect = new RectF(dirtyRect.X + 10, dirtyRect.Y + 10, dirtyRect.Width - 20, dirtyRect.Height - 20);
+      var rect = new RectF(_x + 10, _y + 10, 20, 20);
       canvas.DrawRectangle (rect);
     }
   }
--- Str8tsSolver/Str8tsSolver/MainPage.xaml.cs
+++ Str8tsSolver/Str8tsSolver/MainPage.xaml.cs
@@ -1,18 +1,49 @@
 ﻿using CommunityToolkit.Maui.Core;
 using CommunityToolkit.Maui.Core.Primitives;
+using Microsoft.Maui.Graphics;
+using System.IO;
+using System.Threading;
+using System.Timers;
 
 namespace Str8tsSolver
 {
   public partial class MainPage : ContentPage
   {
     private ICameraProvider _cameraProvider;
+    private int _counter;
+
+    private AutoResetEvent _captureEvent = new AutoResetEvent(true);
+    private CancellationTokenSource _cancellationTokenSource;
+    private Thread _captureThread;
 
     public MainPage(ICameraProvider cp)
     {
       InitializeComponent();
       _cameraProvider = cp;
+      _counter = 0;
 
       MyCamera.MediaCaptured += MyCamera_MediaCaptured;
+
+      // Worker-Thread starten
+      _cancellationTokenSource = new CancellationTokenSource();
+      _captureThread = new Thread(() => CaptureImagesPeriodically(_cancellationTokenSource.Token))
+      {
+        Name = "CaptureThread"
+      };
+      _captureThread.Start();
+    }
+
+    private async Task CaptureImagesPeriodically(CancellationToken token)
+    {
+      Thread.Sleep(2000);
+      while (!token.IsCancellationRequested)
+      {
+        string currentThreadName = Thread.CurrentThread.Name;
+        MyCamera.CaptureImage(CancellationToken.None);
+        _captureEvent.Reset();
+        _captureEvent.WaitOne();
+        Thread.Sleep(10);
+      }
     }
 
     protected async override void OnNavigatedTo(NavigatedToEventArgs args)
@@ -31,11 +62,23 @@ namespace Str8tsSolver
 
       MyCamera.MediaCaptured -= MyCamera_MediaCaptured;
       MyCamera.Handler?.DisconnectHandler();
+
+      // Worker-Thread stoppen
+      //_cancellationTokenSource.Cancel();
     }
 
     private void MyCamera_MediaCaptured(object? sender, CommunityToolkit.Maui.Views.MediaCapturedEventArgs e)
     {
-      //_currentFrame = ReadStreamToMat(e.Media);
+      string currentThreadName = Thread.CurrentThread.Name;
+      var bytes = ReadFromStream(e.Media);
+      if (bytes == null || bytes.Length == 0)
+        return;
+
+      _counter++;
+      myGraphics.UpdatePosition(_counter, _counter);
+      myView.Invalidate();      
+      _captureEvent.Set();
+
       //if (_currentFrame != null)
       //{
       //  // Bildverarbeitung durchführen und Rechteckposition berechnen
@@ -48,6 +91,28 @@ namespace Str8tsSolver
       //  //OverlayGraphicsView.Invalidate();
       //  Dispatcher.Dispatch(() => OverlayGraphicsView.Invalidate());
       //}
+    }
+
+    private byte[] ReadFromStream(Stream stream)
+    {
+      using (var memoryStream = new MemoryStream())
+      {
+        try
+        {
+          stream.CopyTo(memoryStream);
+          byte[] data = memoryStream.ToArray();
+          //Mat mat = new Mat();
+          //CvInvoke.Imdecode(data, ImreadModes.Color, mat);
+          //CvInvoke.Circle(mat, new Point(100, 100), 50, new MCvScalar(0, 0, 255), 2);
+          //SaveRegionToFile(mat, fn);
+          return data;
+        }
+        catch (Exception ex)
+        {
+          Console.WriteLine(ex.Message);
+        }
+      }
+      return null;
     }
   }
 
